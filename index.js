47d342779624110b7f024e923c314ffa6572e3c6
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-07 16:11:18 +0530

    INC_COUNT WORKING

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const action = { type: "", payload: ""}; //Flux Fixed
+const incCountAction = { type: "INC_COUNT", payload: 1}; //Flux Fixed
+
+// const decCountAction = { type: "DEC_COUNT", payload: 1}; //Flux Fixed
+const addTodoAction = { 
+    type: "ADD_TODO", 
+    payload: {id: 1, title: "Learn Redux", status: false},
+}; //Fixed, Payload Optional
 
 class Store {
     constructor(reducer, init) {
         this.reducer = reducer;
-        this.state = init
+        this.state = init;
     }
 
     getState() {
-        return this.state
+        return this.state;
+    }
+
+    dispatch(action){
+        this.state = this.reducer(this.state, action)
     }
 }
 
 //Pure Function
 //Fixed
-const reducer = (store, action) => {
-    return store;
+const reducer = (state, action) => {
+    // if (action.type === "INC_COUNT") {state.count = state.count + action.payload}
+    if (action.type === "INC_COUNT") {
+        return { ...state, count: state.count + action.payload }
+    }
+    return state;
 };
 
-const init = { count: 0};
+const init = { count: 0 };
 
 const store = new Store(reducer, init); // Fixed
+
+console.log(store.getState());
+
+store.dispatch({type: "INC_COUNT", payload: 1})
 
 console.log(store.getState());
