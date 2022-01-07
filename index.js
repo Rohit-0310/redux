1f6ece0d5d888459ff7a7ab78f0d9f1ff93b7eec
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-07 15:41:45 +0530

    ruducer function working

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-index.js;
+const action = { type: "", payload: ""}; //Flux Fixed
+
+class Store {
+    constructor(reducer, init) {
+        this.reducer = reducer;
+        this.state = init
+    }
+
+    getState() {
+        return this.state
+    }
+}
+
+//Pure Function
+//Fixed
+const reducer = (store, action) => {
+    return store;
+};
+
+const init = { count: 0};
+
+const store = new Store(reducer, init); // Fixed
+
+console.log(store.getState());
