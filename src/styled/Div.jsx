bc0f2dcc486a46c1e0cd0010082780f770fedc55
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-01-12 13:30:38 +0530

    final Update

--- src/styled/Div.jsx
+++ src/styled/Div.jsx
@@ -4,14 +4,15 @@ export const Div = styled.div`
     /* display: fles; */
     width: 100%;
     height: 70px;
+    font-size: 24px;
     background-color: #36a5b4;
     justify-content: center;
     color: black;
     margin: 1%;
-    & > p{
-        color: green;
-    }
-    & > p:hover {
-        color: blue;
+    &:hover {
+        color: #112fd4;
+        background-color: #6d2e2e;
+        font-size: 30px;
+        border-radius: 30px;
     }
 `;
